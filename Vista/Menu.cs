c306b90b2d9d3c09cbf058e3f29a3ba19e93965d
Yanqui <rodyanquic@upt.pe>
2020-10-22 19:22:30 -0500

    ProyectoBase

---
+++ Vista/Menu.cs
@@ -0,0 +1,30 @@
+﻿using Controlador;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Vista
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        public void metodo()
+        {
+
+            Class1 nomree = new Class1();
+            
+
+        }
+    }
+}
